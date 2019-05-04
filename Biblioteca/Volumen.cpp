fb3ddda57497809f66a4e8ea7bff6e374bfcf8df
Angel Motta <angelmotta@gmail.com>
2019-05-04 13:36:01 -0500

    Se implementa metodo mostrar en clase padre Volumen

--- Biblioteca/Volumen.cpp
+++ Biblioteca/Volumen.cpp
@@ -11,6 +11,7 @@ void Volumen::mostrar(std::vector<Volumen*> &vector_vols){
     std::cout << "Metodo Clase Padre: Clase Volumen" << std::endl;
     std::vector<Volumen *>::iterator ptr;
     for (ptr = vector_vols.begin(); ptr < vector_vols.end(); ptr++) {
-        std::cout << "VOLUMEN: " << titulo << std::endl;
+        std::cout << "VOLUMEN: " << (*ptr)->titulo << std::endl;
     }
+    std::cout << std::endl;
 }
